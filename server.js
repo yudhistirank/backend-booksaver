0e70773d465e20d6ee3151ed7087187a5fa8ab91
yudhistirank <yudhistiranandakumala@gmail.com>
2025-04-17 13:32:19 +0700

    tambah cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const bookRoutes = require('./routes/bookRoutes');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
